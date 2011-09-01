cf76230f7e4cbb299aa4e5188153c3f618b22194
Huang Gehua <bolixie@gmail.com>
2011-09-02 00:18:02 +0800

    use dns replace ip address

--- src/cn/zadui/reader/helper/NetHelper.java
+++ src/cn/zadui/reader/helper/NetHelper.java
@@ -23,7 +23,7 @@ public class NetHelper {
 	
 	//public static final String HOST_NAME="172.29.1.67";
 //	public static final String HOST_NAME="192.168.1.108";
-	public static final String HOST_NAME="221.122.112.135";
+	public static final String HOST_NAME="meili.51leiju.cn";
 	public static final String PORT="3389";
 	
 	public static String webPath(String protocol,String path){
